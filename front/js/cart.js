88a88c7af213449cd4f451c6846205b200cd43c5
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-09-17 06:47:41 +0200

    Récupération sécurisée des prix à partir de l'API, mise à jour de la quantité totale des articles du panier, calcul et affichage du prix du panier (mise à jour en fonction des évènements à travailler)

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,19 +2,6 @@
 Récupération des articles ajoutés au panier
 */
 
-// 0. Construction de la class Product afin de récupérer et organiser les données brutes de chaque produit en provenance du fichier products de l'API :
-class Product {
-    constructor(productData) {
-        productData && Object.assign(this, productData);        
-    }
-};
-
-class Price {
-    constructor(price) {
-        this.price = price;
-    }
-};
-
 // I. Récupération du panier (sessionStorage) avec l'affichage de chaque article et ses caractéristiques :
 cart = JSON.parse(sessionStorage.getItem("cart"));
 
@@ -22,209 +9,161 @@ if (cart == null || cart == 0) {
     (alert("Votre panier est vide. Merci de retourner à l'accueil pour choisir vos articles !"));
 
     document.getElementById("cart__items").innerHTML +=
-    `
-    <div class="cart__empty">
-        <p>
-            Votre panier est vide.<br> Merci de retourner à l'<a href="./index.html">accueil</a> pour choisir vos articles !
-        </p>
-    </div>
-    `
+        `
+            <div class="cart__empty">
+                <p>
+                    Votre panier est vide.<br> Merci de retourner à l'<a href="./index.html">accueil</a> pour choisir vos articles !
+                </p>
+            </div>
+        `
 }
 
 else {
-    let product = new Product();
-    for (product of cart) {
 
-    // II. Distribution des éléments nécessaires sur la page pour chaque article du panier.
-        document.getElementById("cart__items").innerHTML +=
-            `
-                <article class="cart__item" data-id="${product.selectId}" data-color="${product.selectColor}">
-                    <div class="cart__item__img">
-                        <img src="${product.selectImageUrl}" alt="${product.selectAltTxt}">
-                    </div>
-                    <div class="cart__item__content">
-                        <div class="cart__item__content__description">
-                        <h2>${product.selectName}</h2>
-                        <p>${product.selectColor}</p>
-                        <p>
-                            ${
-                                // III. Récupération sécurisée du prix de chaque article sur l'API :
-                                fetch("http://localhost:3000/api/products/" + product.selectId)
-                                    .then(allProductsData => allProductsData.json())
-                                    .then(getAllProductsPrices => { // getProductPrice
+    totalCartPrice = 0;
 
-                                        console.log(getAllProductsPrices.price); // Récupère bien tous les prix de tous les produits du panier.
-                                        
-                                        let displayPrice = document.querySelector(".cart__item__content__description > p").nextElementSibling;
-                                        displayPrice.innerText = ""; // Si enlevé : tous les prix des produits sur le premier produit.
-                                        displayPrice.innerText += getAllProductsPrices.price; // Prend le prix du dernier article du panier pour l'afficher au premier article. Les autres : [objectPromise]                                        
-                                    })
-                                    .catch((error) => console.error(error))
-                            }
-                        </p>
-                        </div>
-                        <div class="cart__item__content__settings">
-                        <div class="cart__item__content__settings__quantity">
-                            <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.selectQuantity}">
-                        </div>
-                        <div class="cart__item__content__settings__delete">
-                            <p class="deleteItem">Supprimer</p>
-                        </div>
-                        </div>
-                    </div>
-                    </article>
-                
-            `
+    // Affichage de la quantité de tous les articles ajoutés au panier dès l'arrivée sur le panier :
+    let totalQuantity = 0;    
+    for (index in cart) {                                
+        let updateQuantity = parseInt(cart[index].selectQuantity);
+        totalQuantity += updateQuantity;
+
+        
     }
-            
-}
+    document.getElementById("totalQuantity").innerText +=
+        `
+            ${totalQuantity}
+        ` 
 
-// IV. Affichage de la quantité de tous les articles ajoutés au panier
-let totalQuantity = 0;
-for (index in cart) {
-    let updateQuantity = parseInt(cart[index].selectQuantity);
-    totalQuantity += updateQuantity;
-}
-document.getElementById("totalQuantity").innerText +=
-            `
-                ${totalQuantity}
-            `
+    for (let product of cart) {        
+        // II. Répartition de chaque produit et de ses caractéristiques sur la page, avec la récupération sécurisée du prix à partir de l'API :
+        fetch("http://localhost:3000/api/products/" + product.selectId)
+            .then(allProductsData => allProductsData.json())
+            .then(getAllProductsPrices => {                
+                document.getElementById("cart__items").innerHTML +=
+                    `
+                        <article class="cart__item" data-id="${product.selectId}" data-color="${product.selectColor}">
+                            <div class="cart__item__img">
+                                <img src="${product.selectImageUrl}" alt="${product.selectAltTxt}">
+                            </div>
+                            <div class="cart__item__content">
+                                <div class="cart__item__content__description">
+                                <h2>${product.selectName}</h2>
+                                <p>${product.selectColor}</p>
+                                <p>${getAllProductsPrices.price}&nbsp;€</p>
 
-// V. Modification de la quantité pour chaque article et de la quantité totale des articles :
-Array.from(document.getElementsByClassName("itemQuantity")).forEach(node => node.addEventListener("input", changeQuantity => {
-    let itemQuantity = node.closest(".itemQuantity").value;    
-
-    // Blocage des quantités tapées au clavier inférieures à 0 et supérieures à 100
-    if (itemQuantity > 100 || itemQuantity <= -1) { // || itemQuantity <= -1
-        (alert("Merci de bien vouloir choisir une quantité comprise entre 1 et 100 articles maximum !")); // Saut de ligne \n entre les guillements : "Merci de bien vouloir choisir une quantité inférieure à 100 articles !\nRetourner au panier ?"
-        // Remettre la quantité précédente automatiquement à jour.        
-    }    
-
-    else {
-        // Mise à jour de la quantité entre le DOM et le panier (cart du sessionStorage)
-        function adjust() {
-            let itemId = node.closest(".cart__item").dataset.id;
-            let itemColor = node.closest(".cart__item").dataset.color;            
-            let match = cart.find(retrieve => retrieve.selectId == itemId && retrieve.selectColor == itemColor);
-            
-            function replace() {match.selectQuantity = itemQuantity};
-            
-            replace();
-        };
-        function save() {sessionStorage.setItem("cart", JSON.stringify(cart))};    
-        
-        save(adjust());
-        
-        // Mise à jour de la quantité totale du panier sur la page du site
-        let totalQuantity = 0;
-        
-        for (index in cart) {
-            
-            let updateQuantity = parseInt(cart[index].selectQuantity);            
-            totalQuantity += updateQuantity;            
-
-            document.getElementById("totalQuantity").innerText = "";
-            document.getElementById("totalQuantity").innerText +=
-                `
-                    ${totalQuantity}
-                `         
-        } // Si utilisation du clavier, taper entrée.
-
-        // Suppression d'un article avec 0 ou supprimer + entrée au clavier        
-        if (itemQuantity == 0) {
-            let itemId = node.closest(".cart__item").dataset.id;
-            let itemColor = node.closest(".cart__item").dataset.color;            
-            let match = cart.find(retrieve => retrieve.selectId == itemId && retrieve.selectColor == itemColor);
-
-            function discard() {cart.splice(cart.indexOf(match), 1)};    
-            function save() {sessionStorage.setItem("cart", JSON.stringify(cart))};
-            function refresh() {window.location.reload()}
+                            </div>
+                                <div class="cart__item__content__settings">
+                                    <div class="cart__item__content__settings__quantity">
+                                        <p>Qté : </p>
+                                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.selectQuantity}">
+                                    </div>
+                                    <div class="cart__item__content__settings__delete">
+                                        <p class="deleteItem">Supprimer</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>                        
+                    `
                     
-            refresh((save(discard())));
-        }
+                // Calcul du prix de chaque article en fonction de sa quantité.
+                let totalItemPrice = product.selectQuantity * getAllProductsPrices.price;
+                
+                // III. Calcul du coût total du panier et affichage du résultat :
+                totalCartPrice += totalItemPrice;
+                                    
+                document.getElementById("totalPrice").innerText = "";
+                document.getElementById("totalPrice").innerText +=
+                
+                    `
+                        ${totalCartPrice}
+                    `
 
-        // Bug en tapant "e" : supprime l'article. Seule lettre qui s'affiche quand on la tape.
-        
-    }
-}));
-
-// VI. Suppression d'un article avec les touches "enter", "backspace" ou "delete"
-Array.from(document.getElementsByClassName("deleteItem")).forEach(node => node.addEventListener("click", deleteItem => {
-    let itemId = node.closest(".cart__item").dataset.id;
-    let itemColor = node.closest(".cart__item").dataset.color;            
-    let match = cart.find(retrieve => retrieve.selectId == itemId && retrieve.selectColor == itemColor);
-    
-    function discard() {cart.splice(cart.indexOf(match), 1)};    
-    function save() {sessionStorage.setItem("cart", JSON.stringify(cart))};
-    function refresh() {window.location.reload()}    
-    
-    refresh((save(discard()))); // A voir si la suppression du sessionStorage par un clear() ou un removeItem() est utile.
-}));
-
-// VII. Calcul du coût total du panier
-// Chercher la quantité de chaque produit dans le cart OU dans le DOM.
-// Multiplier chaque quantité avec le prix (fetch OU DOM ?).
-
-let articleQuantity = document.getElementsByClassName("itemQuantity").value;
-console.log(articleQuantity); // undefined
-for (product of cart) {
-    console.log(product.selecQuantiy);} // undefined
-
-let itemQuantity = Array.from(document.getElementsByClassName("itemQuantity")).forEach(node => node.closest(".itemQuantity").value); // Si non déclarée : node not defined ; sinon, undefined.    
-
-for (product of cart) {
-    document.getElementById("totalPrice").innerText +=
-        `
-            ${
-                fetch("http://localhost:3000/api/products/" + product.selectId) // Voir si un nouveau fetch est utile.
-                    .then(allProductsData => allProductsData.json())
-                    .then(getAllProductsPrices => {
+                    
+                    document.getElementById("totalPrice").addEventListener("input", changeTotal => {
                         
-                        /* reduce @ MDN
-                        const array1 = [1, 2, 3, 4];
+                    });               
+                
+
+                // IV. Modification et mise à jour de la quantité pour chaque article, ainsi que de de la quantité totale des articles :
+                Array.from(document.getElementsByClassName("itemQuantity")).forEach(node => node.addEventListener("input", changeQuantity => {
+                    let itemQuantity = node.closest(".itemQuantity").value;                    
+
+                    // Blocage des quantités tapées au clavier inférieures à 0 et supérieures à 100
+                    if (itemQuantity > 100 || itemQuantity <= -1) {
+                        (alert("Merci de bien vouloir choisir une quantité comprise entre 1 et 100 articles maximum !")); // Saut de ligne \n entre les guillements : "Merci de bien vouloir choisir une quantité inférieure à 100 articles !\nRetourner au panier ?"
+                        // Remettre la quantité précédente automatiquement à jour.        
+                    }    
+
+                    else {
+                        // Modification de la quantité entre le DOM et le panier (cart du sessionStorage)
+                        function adjust() {
+                            let itemId = node.closest(".cart__item").dataset.id;
+                            let itemColor = node.closest(".cart__item").dataset.color;            
+                            let match = cart.find(retrieve => retrieve.selectId == itemId && retrieve.selectColor == itemColor);
+                            
+                            function replace() {match.selectQuantity = itemQuantity};
+                            
+                            replace();
+                        };
+                        function save() {sessionStorage.setItem("cart", JSON.stringify(cart))};    
                         
-                        // 0 + 1 + 2 + 3 + 4
-                        const initialValue = 0;
-                        const sumWithInitial = array1.reduce(
-                            (previousValue, currentValue) => previousValue + currentValue,
-                            initialValue
-                        );
+                        save(adjust());
+                        
+                        // Affichage de la quantité de tous les articles ajoutés au panier :                        
+                        let totalQuantity = 0;
+                        
+                        for (index in cart) {
+                            
+                            let updateQuantity = parseInt(cart[index].selectQuantity);            
+                            totalQuantity += updateQuantity;
+                            /*
+                            if (itemQuantity == NaN) {
+                                totalQuantity = 0;
+                            } */ // Trouver la solution pour mettre totalQuantity = 0 quand totalQuantity affiche NaN.
 
-                        console.log(sumWithInitial);
-                        // expected output: 10
-                        */
+                            document.getElementById("totalQuantity").innerText = "";
+                            document.getElementById("totalQuantity").innerText +=
+                                `
+                                    ${totalQuantity}
+                                `         
+                        } // Si utilisation du clavier, taper entrée.
+                        
+                        // Suppression d'un article avec les touches "enter", "backspace" ou "delete"
+                        // Suppression d'un article avec 0 ou supprimer + entrée au clavier
+                        // Eviter la suppression avec "backspace" ou "delete" pour pouvoir entrer les chiffres.
+                        if (itemQuantity == 0) { // Quand itemQuantity == 0, supprime l'article. Quand itemQuantity === 0, garde l'article, mais sans la quantité (ou "0" ou "" dans la panier en fonction de la valeur entrée).
+                            let itemId = node.closest(".cart__item").dataset.id;
+                            let itemColor = node.closest(".cart__item").dataset.color;            
+                            let match = cart.find(retrieve => retrieve.selectId == itemId && retrieve.selectColor == itemColor);
 
-                        console.log(getAllProductsPrices.price); // Récupère bien tous les prix de tous les produits du panier.
-                        console.log(product.selectQuantity) // Seulement la quantité du dernier produit ajouté.
-
-                        for (index in cart) {                                
-                            let updateQuantity = parseInt(cart[index].selectQuantity);
-                            console.log(updateQuantity);
-                            console.log(cart[index].selectQuantity);
-                            // let totalCartPrice = 0;
-                            // totalCartPrice = totalCartPrice + getAllProductsPrices.price * updateQuantity;
-
-                            totalItemPrice = getAllProductsPrices.price * updateQuantity;
-                            console.log(totalItemPrice);
-
-                            let totalCartPrice = 0;
-                            totalCartPrice = totalItemPrice // ???
-
-                            document.getElementById("totalPrice").innerText = "";
-                            document.getElementById("totalPrice").innerText += totalCartPrice;
-
-                            console.log(totalCartPrice);
+                            function discard() {cart.splice(cart.indexOf(match), 1)};    
+                            function save() {sessionStorage.setItem("cart", JSON.stringify(cart))};
+                            function refresh() {window.location.reload()}
+                                    
+                            refresh((save(discard())));
                         }
+                        // Bug d'origine en tapant "e" : supprime l'article. Seule lettre qui s'affiche quand on la tape. Prévoir un correctif.
+                    }
+                }));
 
-                        console.log(totalQuantity) // Ok
-                        // console.log(totalCartPrice); // La page doit être rafraîchie pour obtenir le prix.
-                        
-                    })
-                    .catch((error) => console.error(error))
-            }
-        `
-};
+                // V. Suppression d'un article avec le bouton "Supprimer"
+                Array.from(document.getElementsByClassName("deleteItem")).forEach(node => node.addEventListener("click", deleteItem => {
+                    let itemId = node.closest(".cart__item").dataset.id;
+                    let itemColor = node.closest(".cart__item").dataset.color;            
+                    let match = cart.find(retrieve => retrieve.selectId == itemId && retrieve.selectColor == itemColor);
+                
+                    function discard() {cart.splice(cart.indexOf(match), 1)};    
+                    function save() {sessionStorage.setItem("cart", JSON.stringify(cart))};
+                    function refresh() {window.location.reload()};    
+                
+                    refresh((save(discard())));
+                }));
+
+            })
+            .catch((error) => console.error(error))        
+    }    
+}
 
  /*
     addEventListener :
