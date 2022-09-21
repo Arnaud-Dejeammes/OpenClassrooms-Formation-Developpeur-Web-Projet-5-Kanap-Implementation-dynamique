fd5b65af61e8c147cd148b9ef5312dad017f7662
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-09-21 19:12:19 +0200

    Nettoyage du fichier script.js avec la sortie du innerHTML de la boucle

--- front/js/script.js
+++ front/js/script.js
@@ -1,50 +1,28 @@
-/*
-Afficher de manière dynamique sur la page index les informations relatives aux produits fournies par l'API 
-*/
-
-// 0. Construction de la class Product afin de récupérer et organiser les données brutes de chaque produit en provenance du fichier products de l'API :
-class Product {
-    constructor(productData) {
-        productData && Object.assign(this, productData);
-        /* Object.assign : assigne les propriétés de l'objet productData dans l'objet pour chaque instance (this) de la class Product
-        Cette syntaxte évite un constructor énumérant toutes les propriétés (colors, _id, name, price, imageUrl, description, altTxt),
-        ainsi que toutes les instances qui le suivent ("this.colors = colors;", etc.). */
-    }
-}
+// )Affichage dynamique sur la page index des informations relatives aux produits fournies par l'API
 
 // I. Requête de l'API pour obtenir l'ensemble des produits :
 fetch("http://localhost:3000/api/products")
 // II. Récupération de la réponse du service web :
-// (Création promise)
-    .then(allProductsData => allProductsData.json()) // Fonction fléchée (partie gauche) : variable créée (résultat du fetch)    
+    .then(allProductsData => allProductsData.json())
     // III. Parcours de la réponse pour l'insertion de chaque produit (élément) :
-    // (Affichage du contenu json dans l'élément HTML cible id (#items))
     .then(getAllProductsProperties => {
-        for (getProductProperties of getAllProductsProperties) { // Parcourir le tableau et créer une variable getProductProperties pour chaque entrée
-            // Suppression du let getProductProperties of getAllProductsProperties
-            let product = new Product(getProductProperties); // class Product créée à l'étape 0.
-            // L'objet product est créé à partir de la class Product avec toutes les propriétés d'un objet
-            console.log(getAllProductsProperties); // Toutes les information de tous les produits
-            console.log(getProductProperties); // Toutes les propriétés d'un objet particulier
-            console.log(product); // Le produit comme objet
-            console.log(Product); // La class Product (constructor) qui construit l'objet product
-            document.getElementById("items").innerHTML += // Template literals
+        let display = "";
+        for (getProductProperties of getAllProductsProperties) {
+            display +=
                 `
-                    <a href="product.html?id=${product._id}">
+                    <a href="product.html?id=${getProductProperties._id}">
                         <article>
-                            <img src="${product.imageUrl}" alt="${product.altTxt}">
+                            <img src="${getProductProperties.imageUrl}" alt="${getProductProperties.altTxt}">
                                 <h3 class="productName">
-                                    ${product.name}
+                                    ${getProductProperties.name}
                                 </h3>
                                 <p class="productDescription">
-                                    ${product.description}
+                                    ${getProductProperties.description}
                                 </p>
                         </article>
                     </a>
-                ` // Le bactick permet le texte multiligne et la méthode d'interpolation de variable ${}.                
+                `
         };
-    }); // Enlever ";" une fois le ".catch" défini et activé
-    /* 
-    .catch(function(err) {
-        // Survenue d'une erreur
-    }); */
+        document.getElementById("items").innerHTML = display;
+    })    
+    .catch((error) => console.error(error));   
