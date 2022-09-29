df9ff6db17b5d156ba2fabded8aa4858e5acba9f
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2022-09-29 16:08:02 +0200

    Récupération du numéro de commande à partir de l'URL de la page Confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,2 +1,2 @@
-let getOrderId = new URL(window.location.href).searchParams.get("orderId");
+let getOrderId = new URL(window.location.href).searchParams.get("id");
 document.getElementById("orderId").textContent = getOrderId;
